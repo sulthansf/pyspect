014c38a748342bf21ad36e00fa16303f96b43897
Frank J Jiang <frankji@kth.se>
2023-07-25 12:21:51 +0200

    Adding levelset example

--- src/pyspect/rules.py
+++ src/pyspect/rules.py
@@ -1,7 +1,7 @@
 
 from abc import ABC, abstractmethod
 
-class Set(ABC): 
+class Set(ABC):
 
     approximation: str
 
@@ -25,7 +25,7 @@ class Set(ABC):
         ]))
 
     @abstractmethod
-    def empty(self): pass 
+    def empty(self): pass
 
     def complement(self): pass
 
@@ -43,21 +43,78 @@ class Set(ABC):
 
     def control_set(self, point): pass
 
-class HJ(Set):
+class LevelSet(Set):
 
-    array: np.ndarray
+    # assumes being checked:
+    #   - approximation rules
+    #   - matching grids
 
-    def __init__(self, arr, approx):
+    value_function: np.ndarray
 
-        self.array = arr
+    def __init__(self, vf, approx, g, exclude=None):
+        self.value_function = vf
         self.approximation = approx
+        self.g = g
+        self.exclude = exclude
+
+    def empty(self):
+        return np.any(self.value_function <= 0)
+
+    def complement(self):
+        if self.approximation == 'under':
+            approx = 'over'
+        elif self.approximation == 'over':
+            approx = 'under'
+        else:
+            approx = 'exact'
+        return LevelSet(-self.value_function, approx = approx,
+                        g = self.g, exclude = self.exclude)
 
     def union(self, s):
-        return HJ(np.minimum(self.array, s),
-                  approximation='under' if s.approximation == 'under' else 'exact')
+        if self.approximation != 'exact':
+            approx = self.approximation
+        elif s.approximation != 'exact':
+            approx = s.approximation
+        else:
+            approx = 'exact'
+        return LevelSet(np.minimum(self.value_function, s.value_function),
+                        approx = approx, g = self.g, exclude = self.exclude)
+
+    def intersect(self, s):
+        if self.approximation != 'exact':
+            approx = self.approximation
+        elif s.approximation != 'exact':
+            approx = s.approximation
+        else:
+            approx = 'exact'
+        return LevelSet(np.maximum(self.value_function, s.value_function),
+                        approx = approx, g = self.g, exclude = self.exclude)
+
+    def brs(self, timeline, dynamics, save_intermediary=False):
+        # TODO: timeline.array
+        target_and_obs = [self.vf, self.exclude]
+        reach_result = HJSolver(dynamics, self.g, target_and_obs,
+                                timeline.array, {"TargetSetMode": "minVWithV0"},
+                                saveAllTimeSteps=save_intermediary)
+        return LevelSet(reach_result, approx = 'under',
+                        g = self.g, exclude = self.exclude)
+
+    def rci(self, timeline, dynamics, save_intermediary=False):
+        target_and_obs = [-self.vf, self.exclude]
+        avoid_result = HJSolver(dynamics, self.g, target_and_obs,
+                                timeline.array, {"TargetSetMode": "minVWithV0"},
+                                saveAllTimeSteps=save_intermediary)
+        return LevelSet(-avoid_result, approx = 'under',
+                        g = self.g, exclude = self.exclude)
+
+    def frs(self): pass
+
+    def membership(self, point): pass
+
+    def control_set(self, point): pass
 
 
-class Rule(ABC): 
+class Rule(ABC):
 
     name: str
     approximation: str
@@ -67,7 +124,7 @@ class Rule(ABC):
 
         self._apply_approximation
 
-    def _apply_approximation(self): 
+    def _apply_approximation(self):
         """
         1. If default approximation is 'exact', the first child decide type of approximation on parent
         2. If parent is under, no child can be over
@@ -76,15 +133,15 @@ class Rule(ABC):
         for child in self.children:
 
             if self.approximation == 'under':
-                assert child.approximation != 'over' 
+                assert child.approximation != 'over'
             elif self.approximation == 'over':
-                assert child.approximation != 'under' 
+                assert child.approximation != 'under'
 
             self.approximation = child.approximation
 
 class Proposition(Rule):
 
-    def __init__(self, name, approximation='exact'): 
+    def __init__(self, name, approximation='exact'):
         self.name = name
         self.approximation = approximation
 
@@ -136,7 +193,7 @@ class And(Rule):
 
 class Or(Rule): pass
 
-class Eventually(Rule): 
+class Eventually(Rule):
 
     approximation = 'under'
 
@@ -157,11 +214,11 @@ class Eventually(Rule):
         return s
 
 class Always(Rule):
-    pass 
+    pass
 
 class Until(Rule):
-    
-    approximation = 'under' 
+
+    approximation = 'under'
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -189,7 +246,7 @@ if __name__ == '__main__':
                 name='phi1')
 
     s, r = solve(rule1,
-                 p1 = HJ([]), 
+                 p1 = HJ([]),
                  p2 = HJ([], approximation='over'))
 
     if s.empty():
