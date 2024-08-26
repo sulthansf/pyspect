c975630d2e9bddd26e63e2802c039dbae50f2f30
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2024-08-26 18:07:48 +0200

    fix: updating a tlt with _construct_from_tlt now works, found bug in _construct_from_builder

--- src/pyspect/set_builder.py
+++ src/pyspect/set_builder.py
@@ -6,53 +6,75 @@ from .langs.base import ImplClientMeta
 
 __all__ = (
     'SetBuilder',
-    'ABSURD',
-    'Empty',
-    'Set',
-    'AppliedSet',
     'ReferredSet',
+    'AppliedSet',
+    'Set',
+    'ABSURD',
+    'EMPTY',
     'HalfSpaceSet',
     'BoundedSet',
 )
 
-"""
-from abc import ABC, abstractmethod
+class SetBuilder:
+    def __call__(self, impl, **lmap): ...
 
-class _SetBuilder(metaclass=ImplClientMeta):
+    def __repr__(self):
+        cls = type(self)
+        ptr = hash(self)
+        return f'<{cls.__name__} at {ptr:#0{18}x}>'
 
-    @abstractmethod
-    def __call__(self, impl: I, **m: '_SetBuilder') -> R: ... 
+class AbsurdSet(SetBuilder):
+    def __call__(self, impl, **lmap):
+        raise ValueError("Cannot realize the absurd set.")
+ABSURD = AbsurdSet()
+
+class Set(SetBuilder):
+    def __init__(self, arg):
+        self.arg = arg
+    def __call__(self, impl, **lmap):
+        return self.arg
+
+class ReferredSet(SetBuilder):
+    def __init__(self, name):
+        self.name = name
+    def __call__(self, impl, **lmap):
+        sb = lmap.pop(self.name)
+        return sb(impl, **lmap)
+
+class AppliedSet(SetBuilder):
+    def __init__(self, name, *builders):
+        self.name = name
+        self.builders = builders
+    def __call__(self, impl, **m):
+        args = [sb(impl, **m) for sb in self.builders]
+        sb = getattr(impl, self.name)
+        return sb(*args)
+    def find(self, cls):
+        yield from super().find(cls)
+        for sb in self.builders:
+            yield from sb.find(cls)
 
 
-class EmptySet(_SetBuilder):
-    class Impl(ABC):
-        @abstractmethod
-        def empty(self) -> R: ...
 
-    def __call__(self, impl, *args): impl.empty()
+class EmptySet(SetBuilder):
+    def __call__(self, impl, **lmap):
+        return impl.empty()
+EMPTY = EmptySet()
 
-class HalfSpaceSet(_SetBuilder):
-
-    class Impl(ABC):
-        @abstractmethod
-        def plane_cut(self, normal, offset, axes): ...
-
+class HalfSpaceSet(SetBuilder):
     def __init__(self, *args, **kwds):
-        self._args = args
-        self._kwds = kwds
+        self.args = args
+        self.kwds = kwds
+    def __call__(self, impl, **lmap):
+        return impl.plane_cut(*self.args, **self.kwds)
 
-    def __call__(self, impl, **m):
-        return impl.plane_cut(*self._args, **self._kwds)
-
-class BoundedSet(_SetBuilder):
-
-    def __init__(self, **bounds) -> None:
-        self._bounds = bounds
-
-    def __call__(self, impl, **m):
+class BoundedSet(SetBuilder):
+    def __init__(self, **bounds):
+        self.bounds = bounds
+    def __call__(self, impl, **lmap):
         s = impl.complement(impl.empty())
         _bounds = [(vmin, vmax, impl.axis(name))
-                   for name, (vmin, vmax) in self._bounds.items()]
+                   for name, (vmin, vmax) in self.bounds.items()]
         for vmin, vmax, i in _bounds:
             if vmax < vmin and impl.axis_is_periodic(i):
                 upper_bound = impl.plane_cut(normal=[0 if i != j else -1 for j in range(impl.ndim)],
@@ -68,58 +90,3 @@ class BoundedSet(_SetBuilder):
                 axis_range = impl.intersect(upper_bound, lower_bound)
             s = impl.intersect(s, axis_range)
         return s
-"""
-        
-I, R = TypeVar('I'), TypeVar('R')
-SetBuilder = Callable[[I, Dict[str, 'SetBuilder[I, R]']], R]
-
-I, R = TypeVar('I'), TypeVar('R')
-def ABSURD(impl: I, m: Dict[str, 'SetBuilder[I, R]']) -> R:
-    raise ValueError("Cannot realize the absurd set.")
-
-I, R = TypeVar('I'), TypeVar('R')
-def Set(arg: R) -> SetBuilder[I, R]:
-    return lambda impl, **m: arg
-
-I, R = TypeVar('I'), TypeVar('R')
-def ReferredSet(name: str) -> SetBuilder[I, R]:
-    return lambda impl, **m: m.pop(name)(impl, **m)
-
-I, R = TypeVar('I'), TypeVar('R')
-def AppliedSet(name: str, *builders: SetBuilder[I, R]) -> SetBuilder[I, R]:
-    def sb(impl, **m):
-        apply = getattr(impl, f'_apply__{name}')
-        args = [sb(impl, **m) for sb in builders]
-        return apply(*args)
-    return sb
-
-I, R = TypeVar('I'), TypeVar('R')
-def Empty() -> SetBuilder[I, R]:
-    return lambda impl, **m: impl.empty()
-
-I, R = TypeVar('I'), TypeVar('R')
-def HalfSpaceSet(*arg, **kwargs): # -> SetBuilder[ImplPlaneCut[R], R]:
-    return lambda impl, **m: impl.plane_cut(*arg, **kwargs)
-
-I, R = TypeVar('I', ), TypeVar('R')
-def BoundedSet(**bounds: Tuple[float, float]): # -> SetBuilder[_ImplBoundedSet[R], R]:
-    def sb(impl, **m: SetBuilder[I, R]) -> R:
-        s = impl.complement(impl.empty())
-        _bounds = [(vmin, vmax, impl.axis(name))
-                   for name, (vmin, vmax) in bounds.items()]
-        for vmin, vmax, i in _bounds:
-            if vmax < vmin and impl.axis_is_periodic(i):
-                upper_bound = impl.plane_cut(normal=[0 if i != j else -1 for j in range(impl.ndim)],
-                                             offset=[0 if i != j else vmin for j in range(impl.ndim)])
-                lower_bound = impl.plane_cut(normal=[0 if i != j else +1 for j in range(impl.ndim)],
-                                             offset=[0 if i != j else vmax for j in range(impl.ndim)])
-                axis_range = impl.complement(impl.intersect(upper_bound, lower_bound))
-            else:
-                upper_bound = impl.plane_cut(normal=[0 if i != j else -1 for j in range(impl.ndim)],
-                                             offset=[0 if i != j else vmax for j in range(impl.ndim)])
-                lower_bound = impl.plane_cut(normal=[0 if i != j else +1 for j in range(impl.ndim)],
-                                             offset=[0 if i != j else vmin for j in range(impl.ndim)])
-                axis_range = impl.intersect(upper_bound, lower_bound)
-            s = impl.intersect(s, axis_range)
-        return s
-    return sb
--- src/pyspect/tlt.py
+++ src/pyspect/tlt.py
@@ -61,20 +61,19 @@ class APPROXDIR(Enum):
 
 
 
-I, R = TypeVar('I'), TypeVar('R')
-TLTLike = Union[TLTFormula,SetBuilder[I, R],'TLT[I, R]']
+TLTLike = Union[TLTFormula,SetBuilder,'TLT']
 
-I, R = TypeVar('I'), TypeVar('R')
-TLTLikeMap = Dict[str, 'TLTLike[I, R]']
+TLTLikeMap = Dict[str, 'TLTLike']
 
-I, R = TypeVar('I'), TypeVar('R')
-LabellingMap = idict[str, Optional[SetBuilder[I, R]]]
+LabellingMap = idict[str, Optional[SetBuilder]]
    
 
 I, R = TypeVar('I'), TypeVar('R')
 class TLT(Generic[I, R]):
 
     __language__ = Void
+
+    __debug = {}
 
     @classmethod
     def select(cls, lang: Language):
@@ -84,47 +83,67 @@ class TLT(Generic[I, R]):
         cls.__language__ = lang
 
     @classmethod
-    def construct(cls, arg: TLTLike[I, R], **kwds: TLTLike[I, R]) -> 'TLT[I, R]':
-        return (cls._construct_from_tlt(arg, kwds)      if isinstance(arg, TLT) else
-                cls._construct_from_prop(arg, kwds)     if isinstance(arg, str) else
-                cls._construct_from_builder(arg, kwds)  if isinstance(arg, Callable) else
-                cls._construct_from_formula(arg, kwds))
+    def debug(cls, **kwds):
+        for k, v in kwds.items():
+            k = f'__debug_{k}'
+            if v is Ellipsis:
+                cls.__debug.pop(k, None) # remove to set default
+            else:
+                cls.__debug[k] = v
 
     @classmethod
-    def _construct_from_tlt(cls, tlt: 'TLT[I, R]', kwds: TLTLikeMap) -> 'TLT[I, R]':
+    def construct(cls, arg: TLTLike, **kwds: TLTLike) -> 'TLT[I, R]':
+        if cls.__debug: kwds.update(cls.__debug)
+        return (cls._construct_from_tlt(arg, **kwds)      if isinstance(arg, TLT) else
+                cls._construct_from_prop(arg, **kwds)     if isinstance(arg, str) else
+                cls._construct_from_builder(arg, **kwds)  if isinstance(arg, Callable) else
+                cls._construct_from_formula(arg, **kwds))
+
+    @classmethod
+    def _construct_from_tlt(cls, tlt: 'TLT[I, R]', **kwds: TLTLikeMap) -> 'TLT[I, R]':
+        # Hacky debug helpers
+        __debug: dict = {k: kwds.pop(k) for k in list(kwds) if k.startswith('__debug')}
+        __debug_print: bool = __debug.get('__debug_print', False)
+        __debug_indent: int = __debug.get('__debug_indent', 0)
+        if __debug_print:
+            print('> ' * __debug_indent + 'tlt'.ljust(7), kwds)
+            __debug['__debug_indent'] = __debug_indent + 1
+
         # Create new TLT with updated information. With this, we can bind new 
         # TLT-like objects to free variables/propositions. We do not allow 
         # updates to existing propositions. First collect all updatable props.
-        updates = {prop: cls.construct(kwds[prop])
-                   for prop, sb in tlt._lmap.items()
-                   if sb is None and prop in kwds}
-        
-        # If there is something to update, these are the relevant fields
-        # that must be changed.
-        formula = tlt._formula
-        lmap = dict(tlt._lmap)
-        
-        # Do the actual update in tlt with subtlt.
-        for prop, subtlt in updates.items():
-            # 1) Replace the proposition in tlt with the corresponding
-            # formula of subtlt.
-            formula = replace_prop(formula, prop, subtlt._formula)
-            # 2) subtlt may depend on a number of sets, we update lmap
-            # accordingly. This is especially important for next step.
-            lmap.update(subtlt._lmap)
-            # 3) To bind subtlt to tlt, we add it as a referred set. 
-            # We follow the steps 
-        
-        return cls(formula, tlt._builder, tlt._approx, idict(lmap))
+        kwds = {prop: kwds[prop] if prop in kwds else sb
+                for prop, sb in tlt._lmap.items()
+                if prop in kwds or sb is not None}
+
+        # It is best to reconstruct builders in the "top-level" TLT when there
+        # is updates to the tree since we have modified how 
+        return cls._construct_from_formula(tlt._formula, **kwds, **__debug)
 
     @classmethod
-    def _construct_from_prop(cls, prop: str, kwds: TLTLikeMap) -> 'TLT[I, R]':
+    def _construct_from_prop(cls, prop: str, **kwds: TLTLikeMap) -> 'TLT[I, R]':
+        # Hacky debug helpers
+        __debug: dict = {k: kwds.pop(k) for k in list(kwds) if k.startswith('__debug')}
+        __debug_print: bool = __debug.get('__debug_print', False)
+        __debug_indent: int = __debug.get('__debug_indent', 0)
+        if __debug_print: 
+            print('> ' * __debug_indent + 'prop'.ljust(7), kwds)
+            __debug['__debug_indent'] = __debug_indent + 1
+
         formula = (prop,) # Propositions are always terminals
-        return (cls.construct(kwds.pop(prop), **kwds) if prop in kwds else 
+        return (cls.construct(kwds.pop(prop), **kwds, **__debug) if prop in kwds else 
                 cls(formula, ReferredSet(prop), lmap={prop: None}))
 
     @classmethod
-    def _construct_from_builder(cls, sb: SetBuilder[I, R], kwds: TLTLikeMap) -> 'TLT[I, R]':
+    def _construct_from_builder(cls, sb: SetBuilder, **kwds: TLTLikeMap) -> 'TLT[I, R]':
+        # Hacky debug helpers
+        __debug: dict = {k: kwds.pop(k) for k in list(kwds) if k.startswith('__debug')}
+        __debug_print: bool = __debug.get('__debug_print', False)
+        __debug_indent: int = __debug.get('__debug_indent', 0)
+        if __debug_print:
+            print('> ' * __debug_indent + 'builder'.ljust(7), kwds)
+            __debug['__debug_indent'] = __debug_indent + 1
+
         # Assume a formula "_0" where the set exactly represent the prop "_0".
         # In reality, we define a unique ID `uid` instead of "_0". We add one
         # extra level of indirection with a ReferredSet for `uid` and 
@@ -133,32 +152,45 @@ class TLT(Generic[I, R]):
         # (information is not lost as when internally binding constant sets to
         # builder functions).
         uid = '_' + builder_uid(sb)
-        return cls.construct(cls(uid, sb), **kwds)
+        formula = (uid,)
+        return cls(formula, ReferredSet(uid), lmap={uid: sb})
 
     @classmethod
-    def _construct_from_formula(cls, formula: TLTFormula, kwds: TLTLikeMap) -> 'TLT[I, R]':
+    def _construct_from_formula(cls, formula: TLTFormula, **kwds: TLTLikeMap) -> 'TLT[I, R]':
+        # Hacky debug helpers
+        __debug: dict = {k: kwds.pop(k) for k in list(kwds) if k.startswith('__debug')}
+        __debug_print: bool = __debug.get('__debug_print', False)
+        __debug_indent: int = __debug.get('__debug_indent', 0)
+        if __debug_print:
+            print('> ' * __debug_indent + 'formula'.ljust(7), kwds)
+            __debug['__debug_indent'] = __debug_indent + 1
+
         head, *tail = formula
         if tail: # Operator: head = op, tail = (arg1, ...)
-            args = [cls.construct(arg, **kwds) for arg in tail]     # make TLTs of formula args
-            apply = getattr(cls.__language__, f'_apply__{head}')    # `apply` creates a builder for op
-            check = getattr(cls.__language__, f'_check__{head}')    # get approx check of op from lang
+            args = [cls.construct(arg, **kwds, **__debug) for arg in tail]     # make TLTs of formula args
+            try:
+                apply = getattr(cls.__language__, f'_apply__{head}')    # `apply` creates a builder for op
+                check = getattr(cls.__language__, f'_check__{head}')    # get approx check of op from lang
+            except AttributeError as e:
+                print(formula)
+                raise e
             lmaps = [list(arg._lmap.items()) for arg in args]       # collect all labels 
             return cls((head, *[arg._formula for arg in args]),
                        apply(*[arg._builder for arg in args]),
                        check(*[arg._approx for arg in args]),
                        idict(sum(lmaps, [])))
         else: # Terminal: head = prop, tail = ()
-            return cls._construct_from_prop(head, kwds)
+            return cls._construct_from_prop(head, **kwds, **__debug)
 
     _formula: TLTFormula
-    _builder: SetBuilder[I, R]
+    _builder: SetBuilder
     _approx: APPROXDIR
     _lmap: LabellingMap
 
     def __init__(self, formula=..., builder=..., approx=..., lmap=...):
         self._formula = formula if formula is not ... else '_0'
         
-        # If constructed with the absurd set, then the TLT is also absurd, i.e. cannot be realized.
+        # If constructed with the absurd set, then the TLT is also absurd, __debug_indent.e. cannot be realized.
         self._builder = builder if builder is not ... else ABSURD
         
         self._approx = approx if approx is not ... else APPROXDIR.EXACT
